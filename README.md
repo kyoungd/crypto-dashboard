19f0a61dfc554c0c67f1a04f42d41c4ac1fb8f94
Young K <kyoungd@hotmail.com>
2018-10-01 14:01:00 -0700

    basic react component and test local storage

--- README.md
+++ README.md
@@ -9,3 +9,12 @@ npm install styled-components
 # select google font do-hyeon
 https://fonts.google.com/specimen/Do+Hyeon
 
+
+# you can run react code and inject css using the styled-components.
+const ControlButton = styled.div`
+  cursor: pointer;
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;
+    font-size:1.1em;
+  `}
+`
--- src/App.js
+++ src/App.js
@@ -1,35 +1,87 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Logo = styled.div`
   font-size:1.5em;
 `
 
 const ControlButton = styled.div`
+  cursor: pointer;
+  ${props => props.active && css`
+    text-shadow: 0px 0px 60px #03ff03;
+    font-size:1.1em;
+  `}
 `
 
-const AppLayout = styled.div`
+const Bar = styled.div`
   display:grid;
   grid-template-columns: 180px auto 100px 100px;
+  margin-bottom: 40px;
+`
+const Content = styled.div`
+`
+
+const AppLayout=styled.div`
   padding: 40px;
 `
 
+const checkFirstVisit = () => {
+  let cryptoDashData = localStorage.getItem("cryptoDash");
+  if (!cryptoDashData) {
+    return {
+      firstVisit: true,
+      page: 'SETTINGS'
+    }
+  }
+}
+
+
 class App extends Component {
+  state = {
+    page: 'DASHBOARD',
+    ...checkFirstVisit()
+  };
+  displayInDashboard = () => this.state.page === 'DASHBOARD';
+  displayInSettings = () => this.state.page === 'SETTINGS';
+  firstVisitMessage = () => {
+    if (this.state.firstVisit) {
+      return <div>Welcome to the site.  Please select your favorite coins.</div>;
+    }
+  };
+  confirmFavorites = ()=> {
+    localStorage.setItem('cryptoDash', 'test');
+    this.setState({firstVisit: false, page:'DASHBOARD'});
+  };
+  settingsContent = () => {
+    return <div>
+      { this.firstVisitMessage() }
+      <div onClick={()=>this.confirmFavorites() }>
+        Confirm Favorites
+      </div>
+    </div>
+  }
   render() {
     return (
       <AppLayout>
-        <Logo>
-          CRYPTO-DASH
-        </Logo>
-        <div></div>
-        <ControlButton>
-          dashboard
-        </ControlButton>
-        <ControlButton>
-          settings
-        </ControlButton>
-      </AppLayout>
+        <Bar>
+          <Logo>
+            CRYPTO-DASH
+          </Logo>
+          <div></div>
+          { !this.state.firstVisit && (
+            <ControlButton active={this.displayInDashboard()} onClick={()=>{this.setState({page : 'DASHBOARD'})}}>
+              DASHBOARD
+            </ControlButton>
+          )}
+          <ControlButton active={this.displayInSettings()} onClick={()=>{this.setState({page : 'SETTINGS'})}}>
+            SETTINGS
+          </ControlButton>
+        </Bar>
+        <Content>
+          { this.displayInSettings() && this.settingsContent() }
+        </Content>
+      </AppLayout>      
     );
   }
 }
